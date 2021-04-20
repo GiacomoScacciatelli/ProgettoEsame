f8922f4d19a1edb4d7abcd057218e051b29af8d0
giacomoscacciatelli <giacomoscacciatelli@gmail.com>
2021-04-20 11:24:32 +0100

    modifica

--- src/progettoGit/ProgettoGit.java
+++ src/progettoGit/ProgettoGit.java
@@ -16,6 +16,8 @@ public class ProgettoGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello");
+        System.out.println("modifica");
     }
     
 }
